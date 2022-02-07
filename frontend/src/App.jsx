1eaad3a0f5f7849d3930ca621d69b99952b16264
anand pandit <atim4077@gmail.com>
2022-02-07 12:52:59 +0530

    mobile version integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,116 +1,185 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { PlayFill, CircleHalf } from "react-bootstrap-icons";
+import Hammer from 'hammerjs'
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-java";
-import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/ext-beautify";
+import "ace-builds/src-noconflict/ext-options";
 import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/mode-python";
-import "ace-builds/src-noconflict/mode-typescript";
+import "ace-builds/src-noconflict/mode-ruby";
 import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/mode-csharp";
-import "ace-builds/src-noconflict/mode-rust";
-import "ace-builds/src-noconflict/mode-golang";
-import "ace-builds/src-noconflict/mode-jsx";
-import "ace-builds/src-noconflict/mode-tsx";
-import "ace-builds/src-noconflict/mode-dart";
-import "ace-builds/src-noconflict/mode-ruby";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/mode-kotlin";
+import "ace-builds/src-noconflict/mode-swift";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-css";
+
 import "ace-builds/src-noconflict/ext-error_marker";
 import "ace-builds/src-noconflict/ext-emmet";
 import "ace-builds/src-noconflict/ext-spellcheck";
-import "ace-builds/src-noconflict/snippets/javascript";
 import "ace-builds/src-noconflict/snippets/python";
-import "ace-builds/src-noconflict/snippets/rust";
-import "ace-builds/src-noconflict/snippets/golang";
-import "ace-builds/src-noconflict/snippets/dart";
 import "ace-builds/src-noconflict/snippets/ruby";
 import "ace-builds/src-noconflict/snippets/html";
-import "ace-builds/src-noconflict/snippets/jsx";
-import "ace-builds/src-noconflict/snippets/tsx";
-import "ace-builds/src-noconflict/snippets/typescript";
 import "ace-builds/src-noconflict/snippets/java";
 import "ace-builds/src-noconflict/snippets/csharp";
+import "ace-builds/src-noconflict/snippets/c_cpp";
+import "ace-builds/src-noconflict/snippets/kotlin";
+import "ace-builds/src-noconflict/snippets/swift";
+import "ace-builds/src-noconflict/snippets/javascript";
+import "ace-builds/src-noconflict/snippets/css";
 
 import Console from "./components/Console";
 
 function CodeCompiler() {
+
+  useEffect(()=>{
+      const csl=document.getElementById('console')
+      const editor=document.getElementById('editor')
+      const cslHead=document.querySelector('#console-head')
+
+      const thor=new Hammer.Manager(cslHead)
+      const power=new Hammer.Pan()
+      thor.add(power)
+      thor.on('pan', (e)=>{
+        const uniSize=window.innerHeight-e.center.y
+        csl.style.height=uniSize+'px'
+        editor.style.height=(e.center.y-20)+'px'
+
+        console.log(e.center);
+      })
+
+
+  },[])
   const languages = {
-    java: {
-      name: "java",
-      value: `
-import java.io.*;
-
-class GFG {
-	public static void main (String[] args) {
-	System.out.println("Hello World");
-	}
-}
-
-    `,
-    },
-    python: {
+    py: {
       name: "python",
-      value: `print("Hello World")
-    `,
+      value: `print('hello world')`,
+      ext: "py",
+      aceMode: "python",
     },
-    javascript: { name: "javascript", value: `console.log('hello world')` },
     html: {
       name: "html",
-      value: `
-    <h1>hello world</h1>
-    `,
+      value: `<h1>hello world</h1>`,
+      ext: "html",
+      aceMode: "html",
     },
-    typescript: { name: "typescript", value: `` },
-    csharp: {
+    swift: {
+      name: "swift",
+      value: `println('Hello World');`,
+      ext: "swift",
+      aceMode: "swift",
+    },
+    kt: {
+      name: "kotlin",
+      value: `fun main(args: Array<String>){
+        println("Hello World")
+      }
+      `,
+      ext: "kt",
+      aceMode: "kotlin",
+    },
+    rb: {
+      name: "ruby",
+      value: `puts 'Hello World'
+      `,
+      ext: "rb",
+      aceMode: "ruby",
+    },
+    cs: {
       name: "csharp",
       value: `namespace HelloWorld
-    {
-      class Hello {		
-        static void Main(string[] args)
-        {
-          System.Console.WriteLine("Hello World");
+      {
+        class Hello {
+          static void Main(string[] args)
+          {
+            System.Console.WriteLine("Hello World");
+          }
         }
       }
-    }
-    `,
+      `,
+      ext: "cs",
+      aceMode: "csharp",
     },
-    rust: { name: "rust", value: `println!("hello world")` },
-    golang: { name: "golang", value: `println('Hello World");` },
-    tsx: { name: "tsx", value: `console.log('hello world')` },
-    jsx: { name: "tsx", value: `console.log('hello world')` },
-    dart: {
-      name: "dart",
-      value: `main(){
-      print('Hello World');
-    }
-    `,
+    c: {
+      name: "c",
+      value: `#include <stdio.h>
+
+      int main() {
+      printf("Hello World");
+      return 0;
+      }
+      `,
+      ext: "c",
+      aceMode: "c_cpp",
     },
-    ruby: { name: "ruby", value: `` },
+    cpp: {
+      name: "cpp",
+      value: `#include <iostream>
+
+      int main() {
+        std::cout << "Hello World";
+        return 0;
+      }
+      `,
+      ext: "cpp",
+      aceMode: "c_cpp",
+    },
+    java: {
+      name: "java",
+      value: `import java.io.*;
+
+      class GFG {
+          public static void main (String[] args) {
+             System.out.println("Hello World");
+          }
+      }`,
+      ext: "java",
+      aceMode: "java",
+    },
+    css: {
+      name: "css",
+      value: ``,
+      ext: "css",
+      aceMode: "css",
+    },
+    js: {
+      name: "javascript",
+      value: `console.log("Hello World");
+      `,
+      ext: "js",
+      aceMode: "javascript",
+    },
   };
-
-  //the above code is creating a boiler plate of default and it is just an object required for functionality
-
+  const [loading, setLoading] = useState(false);
   const [theme, setTheme] = useState("monokai");
-  const [language, setLanguage] = useState("html");
-
+  const [language, setLanguage] = useState("py");
   const [code, setCode] = useState(languages[language].value);
   const [input, setInput] = useState(``);
-  const [output, setOutput] = useState(`puts 'Hello World'
+  const [output, setOutput] = useState(` >> Hello World'
   `);
-  const [fontSize, setFontSize]=useState(14)
+
+  const [fontSize, setFontSize] = useState(14);
   const [tab, setTab] = useState(2);
 
-  const getOutput = async (e) => {
+  const htmlView = () => {
+    const preview = window.open("", "preview");
+    preview.document.open();
+    preview.document.write(code);
+    preview.document.title = "preview";
+    preview.document.close();
+  };
+  const getOutput = (e) => {
+    setLoading(true);
     e.preventDefault();
     if (language == "html") {
-      const preview = window.open("", "preview");
-      preview.document.open();
-      preview.document.write(code);
-      preview.document.title = "preview";
-      preview.document.close();
+      htmlView();
       return;
     }
     // const {name, email, phone, work, password, cpassword} = user;
-    const res = await fetch("/codecompiler", {
+    fetch("http://localhost:8080/codecompiler", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
@@ -120,34 +189,20 @@ class GFG {
         language: language,
         input: input,
       }),
-    });
-    const data = await res.json();
-
-    if (res.status == 200) {
-      setOutput(data.message.output);
-      return console.log("Barobar hai :", data, "output: ", output);
-    } else {
-      // dispatch({ type: "USER", payload: true })
-      // console.log(data)
-      // setOutput(data)
-      console.log("Error Occured");
-      // window.alert("Login Successful");
-      // const notiftoast =()=> toast.success('Login Successful', {
-      //     position: "top-right",
-      //     autoClose: 5000,
-      //     hideProgressBar: false,
-      //     closeOnClick: true,
-      //     pauseOnHover: true,
-      //     draggable: true,
-      //     progress: undefined,
-      //     });
-      //     notiftoast();
-      // history.push("/")
-    }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setOutput(data.message.output);
+        setLoading(false);
+      });
   };
 
   const handleChange = (e) => {
     setCode(e);
+  };
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
   };
 
   const objToArr = (obj) => {
@@ -160,12 +215,13 @@ class GFG {
   const languageArray = objToArr(languages);
   return (
     <>
+    <div className='h-[5vh] bg-stone-900'></div>
       <AceEditor
         style={{ width: "100vw", height: "80vh" }}
-        placeholder="Placeholder Text"
-        mode={language}
+        placeholder="Lets code..."
+        mode={languages[language].aceMode}
         theme={theme}
-        name="code-editor"
+        name="editor"
         onChange={handleChange}
         fontSize={fontSize}
         showPrintMargin={true}
@@ -178,26 +234,42 @@ class GFG {
           enableSnippets: true,
           showLineNumbers: true,
           tabSize: tab,
-          
+
           enableEmmet: true,
           spellcheck: true,
         }}
       />
-      <Console value={output}>
-        <div>
-          <button className="mx-2 bg-white text-stone-900 font-semibold  text-sm px-2 ">
+      <Console output={output}>
+        <div className=" flex">
+          <button className="m-2 text-stone-200   font-semibold  text-sm px-2 ">
             console
           </button>
-          <button
-            onClick={getOutput}
-            className="mx-2 bg-white text-stone-900 font-semibold  text-sm px-2 "
-          >
-            run
-          </button>
-        </div>
-        <div className="mx-2">
+          {loading ? (
+            <CircleHalf
+              className="animate-spin bg-transparent text-green-400 "
+              size={25}
+            />
+          ) : (
+            <PlayFill
+              className="bg-transparent text-green-400 "
+              size={30}
+              onClick={getOutput}
+            />
+          )}
 
-          <button className="bg-stone-800 text-yellow-200 px-2">{theme}</button>
+          <input
+            type="text"
+            className="px-2 m-2 sm:m-1 text-slate-800"
+            placeholder="your input"
+            onChange={handleInput}
+            value={input}
+          />
+        </div>
+        <div className="mx-2  flex">
+          <button className="bg-stone-800 mx-2 text-yellow-200 px-2">
+            {theme}
+          </button>
+
           <select
             className="bg-stone-800 mx-2 pr-2"
             onChange={(e) => {
@@ -211,21 +283,24 @@ class GFG {
             <option value="8">tabSize: 8</option>
           </select>
 
-
-
           <select
             className="bg-stone-800"
             onChange={(e) => {
               let lang = languages[e.target.value];
+
               setCode(lang.value);
               setOutput("");
               setInput("");
-              setLanguage(lang.name);
+              setLanguage(e.target.value);
             }}
             value={language}
           >
-            {languageArray.map((a) => {
-              return <option>{a.name}</option>;
+            {languageArray.map((a, index) => {
+              return (
+                <option key={index} value={a.ext}>
+                  {a.name}
+                </option>
+              );
             })}
           </select>
           <button className="text-stone-100 mx-2">utf-8</button>
--- frontend/src/components/Console.jsx
+++ frontend/src/components/Console.jsx
@@ -1,15 +1,17 @@
+const Console = (props) => {
+  return (
+    <>
+      <div id='console' className="h-[15vh] fixed bottom-0 w-full bg-stone-800 text-white">
+        <nav id='console-head' className="p-1 whitespace-nowrap w-full h-[2.5rem] overflow-x-auto sm:flex sm:justify-between bg-stone-900">
+          {props.children}
+        </nav>
+        <div className="m-2 overflow-scroll pb-8 h-[16vh]">
+        {props.output}
 
-const Console=(props)=>{
-    return (
-        <>
-<div className="h-[20vh] fixed bottom-0 w-full bg-stone-800 text-white">
-    <nav className="p-1 flex justify-between bg-stone-900">
-        {props.children}
-    </nav>
-{props.output}
-</div>
-        </>
-    )
-}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Console
+export default Console;
